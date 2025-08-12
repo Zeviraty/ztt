5676cf1900f13ba5e4f0d31327f5b1e6e1acce24
zeviraty <zeviraty@gmail.com>
2025-08-12 12:09:54 +0200

    Commands

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import zte as term
 import signal
 
+global EXIT
+EXIT = False
+
+def run_command(command: str):
+    global EXIT
+    match command:
+        case "Q" | "q!": EXIT = True
+        case "q": quit()
+
+
+def quit():
+    global EXIT
+    text = "Do you really want to quit?"
+    term.border("╭","╮","╰","╯",w=len(text)+2,h=5,x=term.wh()[0] // 2 - (len(text) // 2), y=term.wh()[1] // 2)
+    term.ip(term.wh()[0]//2-(len(text)//2)+1,term.wh()[1]//2+1,text)
+    term.ip(term.wh()[0]//2-(len(text)//2)+3,term.wh()[1]//2+3,"    |y| Yes  |n| No")
+
+    key = term.getch()
+    statusbar(key)
+    if key == "y":
+        EXIT = True
+    else:
+        for i in range(5):
+            term.ip(term.wh()[0] // 2 - (len(text) // 2),term.wh()[1] // 2+i," " * (len(text)+2))
+
 def on_resize(_,__):
     term.cs()
     h = False
-    mode = "Normal"
+    mode = "SONG"
     border(h)
     statusbar()
     term.ip(3,0,"┤ZTT├")
@@ -12,19 +37,39 @@ def on_resize(_,__):
 def border(h):
     term.border("╭","╮","╰","╯",h=term.wh()[1]-1)
 
-def statusbar(mode:str = "NORMAL",key: str =" ", file:str = "Empty", saved:bool = True):
-    term.ip(3,term.wh()[1],f"-- {mode} --   ")
-    term.ip(term.wh()[0]//2,term.wh()[1],f"{file}{' [+]' if not saved else ''}")
-    term.ip(term.wh()[0]-3,term.wh()[1],f'({key})')
+def statusbar(mode:str = "SONG",key: str =" ", file:str = "Empty", saved:bool = True, command:bool = False):
+    if not command:
+        term.ip(3,term.wh()[1],f"-- {mode} --   ")
+        term.ip(term.wh()[0]//2,term.wh()[1],f"{file}{' [+]' if not saved else ''}")
+        term.ip(term.wh()[0]-3,term.wh()[1],f'({key})')
+    else:
+        term.ip(1,term.wh()[1], " "*(term.wh()[0]-1))
+        term.ip(1,term.wh()[1],":")
+        command = ""
+        while True:
+            key = term.getch()
+            if key == "\r":
+                term.ip(1,term.wh()[1], " "*(term.wh()[0]-1))
+                statusbar(mode,key,file,saved)
+                term.ip(1,term.wh()[1]," ")
+                run_command(command)
+                break
+            elif key == '\x7f':
+                command = command[:-1]
+                term.ip(2+len(command),term.wh()[1]," ")
+            elif len(key) == 1 and 32 <= ord(key) <= 126:
+                command += key
+            term.ip(2,term.wh()[1],command)
 
 def main():
+    global EXIT
     term.echo(False)
     term.raw(True)
     term.hide()
     term.cs()
 
     h = False
-    mode = "NORMAL"
+    mode = "SONG"
     border(h)
     statusbar(mode)
     term.ip(3,0,"┤ZTT├")
@@ -32,33 +77,27 @@ def main():
     while True:
         # Key
         key = term.getch()
-        statusbar(mode,key)
         match key:
-            case "q":
-                text = "Do you really want to quit?"
-                term.border("╭","╮","╰","╯",w=len(text)+2,h=5,x=term.wh()[0] // 2 - (len(text) // 2), y=term.wh()[1] // 2)
-                term.ip(term.wh()[0]//2-(len(text)//2)+1,term.wh()[1]//2+1,text)
-                term.ip(term.wh()[0]//2-(len(text)//2)+3,term.wh()[1]//2+3,"    |y| Yes  |n| No")
-
-                key = term.getch()
-                statusbar(mode,key)
-                if key == "y":
-                    break
-                else:
-                    for i in range(5):
-                        term.ip(term.wh()[0] // 2 - (len(text) // 2),term.wh()[1] // 2+i," " * (len(text)+2))
+            case "q": quit()
             case "?":
                 h = not h
                 border(h)
 
             case '\x1b':
-                mode = "NORMAL"
+                mode = "SONG"
 
             case 'p':
                 mode = "PATTERN"
 
-            case 's':
-                mode = "SONG"
+            case ':':
+                statusbar(mode,key,command=True)
+
+            #case _:
+            #    print(repr(key))
+
+        statusbar(mode,key)
+        if EXIT == True:
+            break
 
     term.cs()
     term.echo(True)
